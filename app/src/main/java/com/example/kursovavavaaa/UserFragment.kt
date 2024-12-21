38011738abd61f59a7941ddf4fe024c85f07f55a
BondarenkoRodion <jamesbolivardigriz2016@gmail.com>
2024-12-21 23:11:13 +0200

    Кнопка зміни облікового запису тепер працює

--- app/src/main/java/com/example/kursovavavaaa/UserFragment.kt
+++ app/src/main/java/com/example/kursovavavaaa/UserFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import com.example.kursovavavaaa.databinding.FragmentSettingsBinding
+import com.example.kursovavavaaa.databinding.FragmentUserBinding
 
 class UserFragment : Fragment() {
+    private lateinit var _binding: FragmentUserBinding
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_user, container, false)
+
+        _binding = FragmentUserBinding.inflate(inflater, container, false)
+
+        binding.editBtn.setOnClickListener {
+
+            val navController = findNavController()
+            navController.navigate(R.id.editProfileFragment)
+        }
+        return binding.root
     }
 
 }
